d3cfedcb2539f44c1572079850d67c2010a91e13
Craig Benjamin Rafter <craigrafter@ip-036-123.vpn.soton.ac.uk>
2016-06-21 12:57:43 +0100

    update test1 d

--- exc1/exc1.cu
+++ exc1/exc1.cu
@@ -10,8 +10,11 @@ __global__ void helloWorld(float f)
 
 int main() 
 {
+	dim3 grid(2, 2, 1);
+	dim3 block(2, 2, 1);
 	/*helloWorld<<<1, 10>>>(1.2345f);*/ 
-	helloWorld<<<2, 5>>>(1.2345f); 
+	/*helloWorld<<<2, 5>>>(1.2345f);*/ 
+	helloWorld<<<grid, block>>>(1.2345f); 
 	cudaDeviceReset();
 	return 0;
 }
