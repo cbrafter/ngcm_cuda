809ffca3d3e4ef8cc6c5fb001fe4469c74ed7663
Craig Benjamin Rafter <craigrafter@ip-036-123.vpn.soton.ac.uk>
2016-06-21 12:51:51 +0100

    update test1 b

--- exc1/exc1.cu
+++ exc1/exc1.cu
@@ -3,7 +3,9 @@
 __global__ void helloWorld(float f) 
 {
 	/*printf("Hello thread %d, f=%f\n", threadIdx.x, f);*/
-	printf("Hello block %i running thread %i, f=%f\n", blockIdx.x, threadIdx.x, f);
+	/* printf("Hello block %i running thread %i, f=%f\n", blockIdx.x, threadIdx.x, f);*/
+	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+	printf("Hello block %i running thread %i, f=%f\n", blockIdx.x, idx, f);
 }
 
 int main() 
