87bc98c0ad1ea015f15457cfcdf247017a4af6fb
Craig Benjamin Rafter <craigrafter@ip-036-123.vpn.soton.ac.uk>
2016-06-21 12:46:10 +0100

    update test1 a

--- exc1/exc1.cu
+++ exc1/exc1.cu
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 __global__ void helloWorld(float f) 
 {
-	printf("Hello thread %d, f=%f\n", threadIdx.x, f);
+	/*printf("Hello thread %d, f=%f\n", threadIdx.x, f);*/
+	printf("Hello block %i running thread %i, f=%f\n", blockIdx.x, threadIdx.x, f);
 }
 
 int main() 
